d6b1c4ee52babcdfc563d2862d7eb3b28e5e5ebd
Ashutosh Senapati <ashutosh_senapati@mckinsey.com>
2023-03-09 21:57:58 +0530

    fix: cors issue

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { router } = require('./routes/routes');
+const cors = require('cors');
 const app = express();
 const PORT = 8080;
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 app.listen(PORT, () => {
